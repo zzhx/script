4191befe8dc5fd05f78e4c7d4367003e60bb60a2
liuyx <liuyx@lensyn.com>
2022-02-14 13:35:30 +0800

    11

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>{{ msg }}</h1>
+    </div>
 </template>
+<script lang="ts" setup>
+let msg: unknown = "为什么啊";
+
+function createArray<T = string>(length: number, value: T): Array<T> {
+    let result: T[] = [];
+    for (let i = 0; i < length; i++) {
+        result[i] = value;
+    }
+    return result;
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        {{age02}} {{sex02}}
+        <compositionapi
+            v-model="sex02"
+            v-model:age="age02"
+            @test="getData"
+            :keywords="keywords"
+        ></compositionapi>
+    </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script lang="ts" setup>
+import { ref } from "vue";
+import compositionapi from "@/components/HelloWorld.vue";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+let age02 = ref(1111);
+let sex02 = ref("nan");
 </script>
